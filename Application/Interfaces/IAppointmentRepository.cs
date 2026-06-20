463e34239fe7cd49229972b00c72bf3fc7428a5e
programador3-globaltec <programador3@globaltecperu.com>
2026-06-20 12:46:26 -0500

    implementacion de interfaz cita

---
+++ Application/Interfaces/IAppointmentRepository.cs
@@ -0,0 +1,12 @@
+﻿using Barberia.Backend.Domain.Entities;
+using Barberia.Backend.Domain.Enums;
+
+namespace Barberia.Backend.Application.Interfaces
+{
+    public interface IAppointmentRepository
+    {
+        Task<Appointment> CreateAsync(Appointment appointment);
+        Task<List<Appointment>> GetByStatusAsync(AppointmentStatus status);
+        Task<Appointment> UpdateAsync(Appointment appointment);
+    }
+}
