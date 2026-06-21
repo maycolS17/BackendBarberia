2e8e797ab78021b5414eca85d9c03a2f3a64e1b0
Maycol <maycolsteven55@gmail.com>
2026-06-21 02:54:31 -0500

    creacion de interfaces planeadas para MVP

--- Application/Interfaces/IAppointmentRepository.cs
+++ Application/Interfaces/IAppointmentRepository.cs
@@ -7,6 +7,8 @@ namespace Barberia.Backend.Application.Interfaces
     {
         Task<Appointment> CreateAsync(Appointment appointment);
         Task<List<Appointment>> GetByStatusAsync(AppointmentStatus status);
-        Task<Appointment> UpdateAsync(Appointment appointment);
+        Task<Appointment> UpdateStatusAsync(int id, AppointmentStatus status);
+        Task<Appointment?> GetByIdAsync(int id);
+        Task<List<Appointment>> GetByBarberAndWorkDateAsync(int barberId, DateOnly workDate);
     }
 }
