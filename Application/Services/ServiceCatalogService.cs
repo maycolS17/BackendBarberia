d0813debd3d978ac3ee6050c6095669995c5f532
Maycol <maycolsteven55@gmail.com>
2026-06-27 01:24:13 -0500

    creacion de casos de uso de servicios

---
+++ Application/Services/ServiceCatalogService.cs
@@ -0,0 +1,41 @@
+﻿using Barberia.Backend.Application.Interfaces;
+using Barberia.Backend.Domain.Entities;
+
+namespace Barberia.Backend.Application.Services
+{
+    public class ServiceCatalogService
+    {
+        private readonly IServiceRepository _serviceRepository;
+
+        public ServiceCatalogService(IServiceRepository serviceRepository)
+        {
+            _serviceRepository = serviceRepository;
+        }
+
+        public async Task<Service> GetByIdAsync(int id)
+        {
+            //Busco el servicio
+            var service = await _serviceRepository.GetByIdAsync(id);
+
+            //Validar que exista
+            if (service == null)
+                throw new Exception("Servicio no encontrado.");
+
+            //Validar que este activo
+            if (!service.IsActive)
+                throw new Exception("El servicio esta inactivo.");
+
+            //devuelvo servicios
+            return service;
+        }
+
+        public async Task<List<Service>> GetActiveAsync()
+        {
+            //Pido servicios activos
+            var services = await _serviceRepository.GetActiveAsync();
+
+            //devuelvo servicios activos
+            return services;
+        }
+    }
+}
