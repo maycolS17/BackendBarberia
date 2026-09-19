df0d6a2866d83b37d5ee94d2a27c2448365d41ea
Maycol <maycolsteven55@gmail.com>
2026-09-19 02:03:43 -0500

    feat: configurar AppDbContext para uso general y creacion de la clase BarberRepository

--- Infrastructure/Persistence/AppDbContext.cs
+++ Infrastructure/Persistence/AppDbContext.cs
@@ -1,6 +1,19 @@
-﻿namespace Barberia.Backend.Infrastructure.Persistence
+﻿using Microsoft.EntityFrameworkCore;
+using Barberia.Backend.Domain.Entities;
+
+namespace Barberia.Backend.Infrastructure.Persistence
 {
-    public class DbContext
+    public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Appointment> Appointments { get; set; } = null!;
+        public DbSet<Barber> Barbers { get; set; } = null!;
+        public DbSet<Client> Clients { get; set; } = null!;
+        public DbSet<Schedule> Schedules { get; set; } = null!;
+        public DbSet<Service> Services { get; set; } = null!;
     }
 }
---
+++ Infrastructure/Repository/BarberRepository.cs
@@ -0,0 +1,25 @@
+﻿using Barberia.Backend.Application.Interfaces;
+using Barberia.Backend.Domain.Entities;
+using Barberia.Backend.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace Barberia.Backend.Infrastructure.Repository
+{
+    public class BarberRepository : IBarberRepository
+    {
+        private readonly AppDbContext _context;
+        public BarberRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Barber>> GetActiveAsync()
+        {
+            return await _context.Barbers.Where(b => b.IsActive).ToListAsync();
+        }
+        public async Task<Barber?> GetByIdAsync(int id)
+        {
+            return await _context.Barbers.FirstOrDefaultAsync(b => b.Id == id);
+        }
+    }
+}
