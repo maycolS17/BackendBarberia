0a6454038e0bbeed48253cd0b289d814fe4d376c
Maycol <maycolsteven55@gmail.com>
2026-09-22 01:50:08 -0500

    feat: implementar AppointmentRepository y ScheduleRepository

---
+++ Infrastructure/Repository/AppointmentRepository.cs
@@ -0,0 +1,52 @@
+﻿using Barberia.Backend.Application.Interfaces;
+using Barberia.Backend.Domain.Entities;
+using Barberia.Backend.Domain.Enums;
+using Barberia.Backend.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace Barberia.Backend.Infrastructure.Repository
+{
+    public class AppointmentRepository : IAppointmentRepository
+    {
+        private readonly AppDbContext _context;
+        public AppointmentRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Appointment> CreateAsync(Appointment appointment)
+        {
+            _context.Appointments.Add(appointment);
+            await _context.SaveChangesAsync();
+            return appointment;
+        }
+        public async Task<List<Appointment>> GetByStatusAsync(AppointmentStatus status)
+        {
+            return await _context.Appointments.Where(a => a.Status == status).ToListAsync();
+        }
+        public async Task<Appointment> UpdateStatusAsync(int appointmentId, AppointmentStatus status)
+        {
+            var appointment = await _context.Appointments.FirstOrDefaultAsync(a => a.Id == appointmentId);
+
+            if (appointment == null)
+            {
+                throw new Exception($"Cita con ID {appointmentId} no encontrada.");
+            }
+            appointment.Status = status;
+
+            await _context.SaveChangesAsync();
+            return appointment;
+        }
+        public async Task<Appointment?> GetByIdAsync(int id)
+        {
+            return await _context.Appointments.FirstOrDefaultAsync(a => a.Id == id);
+        }
+        public async Task<List<Appointment>> GetByBarberAndWorkDateAsync(int barberId, DateOnly workDate)
+        {
+            var startOfDay = workDate.ToDateTime(TimeOnly.MinValue);
+            var endOfDay = startOfDay.AddDays(1);
+
+            return await _context.Appointments.Where(a => a.BarberId == barberId && a.StartDateTime >= startOfDay && a.StartDateTime < endOfDay).ToListAsync();
+        }
+    }
+}
---
+++ Infrastructure/Repository/ScheduleRepository.cs
@@ -0,0 +1,26 @@
+﻿using Barberia.Backend.Application.Interfaces;
+using Barberia.Backend.Domain.Entities;
+using Barberia.Backend.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace Barberia.Backend.Infrastructure.Repository
+{
+    public class ScheduleRepository : IScheduleRepository
+    {
+        private readonly AppDbContext _context;
+        public ScheduleRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Schedule?> GetByIdAsync(int id)
+        {
+            return await _context.Schedules.FirstOrDefaultAsync(s => s.Id == id);
+        }
+
+        public async Task<List<Schedule>> GetActiveByBarberAndWorkDateAsync(int barberId, DateOnly workDate)
+        {
+            return await _context.Schedules.Where(s => s.IsActive && s.BarberId == barberId && s.WorkDate == workDate).ToListAsync();
+        }
+    }
+}
